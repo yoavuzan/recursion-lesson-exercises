5149aa938340d308d5ffa9b20ee448c4c6ee2530
danbrud <dannybrudner@users.noreply.github.com>
2019-09-10 13:56:56 +0300

    changed function name

--- tests/exercises/ex-1.test.js
+++ tests/exercises/ex-1.test.js
@@ -4,10 +4,10 @@ describe('Exercise 1', function () {
     it('The function should return the factorial of the argument that has been passed to it', function () {
         const param = 10
         const expectedResult = 3628800
-        const factorial = jest.spyOn(recursionExercises, 'findFactorial')
-        const result = factorial(param)
+        const findFactorial = jest.spyOn(recursionExercises, 'findFactorial')
+        const result = findFactorial(param)
 
         expect(result, `the function did not return the correct value, when passing ${param} to the function expected ${expectedResult} got  ${result}`).toBe(expectedResult)
-        expect(factorial, `function must be called ${param} times`).toHaveBeenCalledTimes(param)
+        expect(findFactorial, `function must be called ${param} times`).toHaveBeenCalledTimes(param)
     })
 } )
