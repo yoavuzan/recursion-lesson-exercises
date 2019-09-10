fb60514239d4239a18408ea76d6b4e08e1d860af
danbrud <dannybrudner@users.noreply.github.com>
2019-09-10 12:03:02 +0300

    completed testcd /Users/danny/code/elevation/tests/recursion-lesson-exercises-solutions ; /Users/danny/.nvm/versions/node/v10.16.0/bin/node --inspect-brk=11124 node_modules/.bin/jest tests/exercises/ex-1.test.js --config=./jest.config.js

--- tests/exercises/ex-1.test.js
+++ tests/exercises/ex-1.test.js
@@ -1,15 +1,13 @@
-const ex = require('../../src/app')
-const findFactorial = ex.findFactorial.bind(ex)
-console.log(findFactorial(10))
+const recursionExercises = require('../../src/app')
 
 describe('Exercise 1', function () {
     it('The function should return the factorial of the argument that has been passed to it', function () {
         const param = 10
         const expectedResult = 3628800
-        const factorial = jest.fn(findFactorial)
+        const factorial = jest.spyOn(recursionExercises, 'findFactorial')
         const result = factorial(param)
 
         expect(result, `the function did not return the correct value, when passing ${param} to the function expected ${expectedResult} got  ${result}`).toBe(expectedResult)
-        expect(factorial).toHaveBeenCalledTimes( param - 1 )
+        expect(factorial).toHaveBeenCalledTimes(param)
     })
 } )
