faaf40024d2846403db54fe76a458a2c84b89469
danbrud <dannybrudner@users.noreply.github.com>
2019-09-10 10:44:52 +0300

    wrote test for ex 1 and changed functions to be in a class

--- src/app.js
+++ src/app.js
@@ -1,42 +1,38 @@
-//Exercise 1
-const findFactorial = function(num, currentPorduct = num) {
-  //Your code here
-  if (num === 1 || num < 1) { return currentPorduct }
+class RecursionExercises {
+  //Exercise 1
+  findFactorial (num, currentPorduct = num) {
+    //Your code here
+    if (num === 1 || num < 1) { return currentPorduct }
+    console.log("run")
+    currentPorduct *= num - 1
+    return this.findFactorial(--num, currentPorduct)
+  }
 
-  currentPorduct *= num - 1
-  return findFactorial(--num, currentPorduct)
-}
-
-console.log(findFactorial(8))
-
-//Exercise 2
-const reverseString = function (str, newStr = '') {
+  //Exercise 2
+  reverseString(str, newStr = '') {
     //Your code here
     if(str.length === 0) { return newStr }
 
     newStr += str.slice(str.length - 1)
     str = str.slice(0, str.length - 1)
 
-    return reverseString(str, newStr)
-}
+    return this.reverseString(str, newStr)
+  }
 
-console.log(reverseString("butter"))
-
-//Exercise 3
-const arr1 = [1, 2, 3]
-const arr2 = []
-
-const swap = function (arr1, arr2) {
+  //Exercise 3
+  swap(arr1, arr2) {
     //Your code here
     if(arr1.length === 0) { return }
 
     arr2.push(arr1.splice(0,1)[0])
-    return swap(arr1, arr2)
+    return this.swap(arr1, arr2)
+  }
 }
 
-swap(arr1, arr2)
-console.log(arr1) //[]
-console.log(arr2) //[1, 2, 3]
+const ex = new RecursionExercises()
+
+const arr1 = [1, 2, 3]
+const arr2 = []
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
-module.exports = { findFactorial, reverseString, swap }
+module.exports = ex
