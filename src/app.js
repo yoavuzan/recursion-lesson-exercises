24640917c0fcf53bd0b5326793f0780c7736e925
danbrud <dannybrudner@users.noreply.github.com>
2019-09-11 09:29:54 +0300

    removed tests and modified instructions [ci skip]

--- src/app.js
+++ src/app.js
@@ -1,31 +1,26 @@
+/*
+  Write your code in the corresponding method
+  Please note: You must also add the correct arguments to the methods
+*/
+
+
 const recursionExercises = {
   //Exercise 1
-  findFactorial: function (num, currentPorduct = num) {
+  findFactorial: function () {
     //Your code here
-    if (num === 1 || num < 1) { return currentPorduct }
 
-    currentPorduct *= num - 1
-    return recursionExercises.findFactorial(--num, currentPorduct)
   },
 
   //Exercise 2
-  reverseString: function (str, newStr = '') {
+  reverseString: function () {
     //Your code here
-    if(str.length === 0) { return newStr }
 
-    newStr += str.slice(str.length - 1)
-    str = str.slice(0, str.length - 1)
-
-    return recursionExercises.reverseString(str, newStr)
   },
 
   //Exercise 3
-  swap: function (arr1, arr2) {
+  swap: function () {
     //Your code here
-    if(arr1.length === 0) { return }
 
-    arr2.push(arr1.splice(0,1)[0])
-    return recursionExercises.swap(arr1, arr2)
   }
 }
 
