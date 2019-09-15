04616e9b8c7801d109fb874821ee03462391a522
danbrud <dannybrudner@users.noreply.github.com>
2019-09-15 11:45:30 +0300

    fixed tests

--- tests/exercises/ex-1.test.js
+++ tests/exercises/ex-1.test.js
@@ -1,13 +1,11 @@
-const findFactorial = require('../../src/app')
+const findFactorial = require('../../src/app').findFactorial
 
 describe('Exercise 1', function () {
     it('The function should return the factorial of the argument that has been passed to it', function () {
         const param = 10
         const expectedResult = 3628800
-        const findFactorial = jest.spyOn(recursionExercises, 'findFactorial')
         const result = findFactorial(param)
 
         expect(result, `the function did not return the correct value, when passing ${param} to the function expected ${expectedResult} got ${result}`).toBe(expectedResult)
-        expect(findFactorial, `function must be called ${param} times`).toHaveBeenCalledTimes(param)
     })
 } )
--- tests/exercises/ex-3.test.js
+++ tests/exercises/ex-3.test.js
@@ -1,20 +1,15 @@
-const recursionExercises = require('../../src/app')
+const swap = require('../../src/app').swap
 
 describe('Exercise 3', function () {
     it('The function should recieve two arrays and transfer all the elements in the first array to the second array so that the second array retains the order from the first array', function () {
       const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
       const arr2 = []
-      const timesToBeCalled = arr1.length + 1
 
-      const swap = jest.spyOn(recursionExercises, 'swap')
       swap(arr1, arr2)
 
       expect(arr1.length, `the function did not swap the arrays, when passing ${JSON.stringify(arr1)} as arr1 to the function expected length of arr1 to be 0 got ${arr1.length}`).toBe(0)
       expect(arr2.length, `the function did not swap the arrays, when passing ${JSON.stringify(arr2)} as arr1 to the function expected length of arr1 to be 9 got ${arr2.length}`).toBe(9)
-
       expect(arr1, `the function did not swap the arrays, when passing ${JSON.stringify(arr1)} as arr1 to the function expected arr1 to be [] but got ${JSON.stringify(arr1)}`).toEqual([])
       expect(arr2, `the function did not swap the arrays, when passing ${JSON.stringify(arr2)} as arr2 to the function expected arr2 to be [1,2,3,4,5,6,7,8,9] but got ${JSON.stringify(arr2)}`).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-      expect(swap, `function must be called ${timesToBeCalled} times`).toHaveBeenCalledTimes(timesToBeCalled)
     })
 } )
